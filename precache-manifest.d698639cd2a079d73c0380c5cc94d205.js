02ca072cfab287a88e2f91cba844381b773ae819
Juniel Filappi <juniel123123@outlook.com>
2023-01-30 18:37:16 -0400

    Updates

---
+++ precache-manifest.d698639cd2a079d73c0380c5cc94d205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa1251ab0d7cf48e3d2a08f55359f8d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a1cf0e9ee2f246e2197b",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8ebbdcaaab7b057e0dc4",
+    "url": "/portfolio/static/css/main.2eaae5b1.chunk.css"
+  },
+  {
+    "revision": "a1cf0e9ee2f246e2197b",
+    "url": "/portfolio/static/js/2.48907396.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.48907396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebbdcaaab7b057e0dc4",
+    "url": "/portfolio/static/js/main.2d5b579a.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
